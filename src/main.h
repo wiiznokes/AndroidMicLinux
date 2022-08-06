4a246e9b6f66c618a6897aeaceb70b0ad71b4988
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2022-08-06 02:38:34 +0200

    ve

--- src/main.h
+++ src/main.h
@@ -2,14 +2,14 @@
 #define MAIN_H_
 
 
-// g++ -o prog ./src/*/*.cpp -lusb-1.0 -lpulse-simple -lpulse && ./prog
+// g++ -o prog ./src/*.cpp -lusb-1.0 -lpulse-simple -lpulse && ./prog
 
 #include <iostream>
 #include <libusb-1.0/libusb.h>
 
-#include "audio/audioManager.h"
-#include "connexion/connexion.h"
-#include "stream/transfer.h"
+#include "audioManager.h"
+#include "connexion.h"
+#include "transfer.h"
 
 
 
